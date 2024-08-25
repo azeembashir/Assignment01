1d2b7ac53492afb9b28d556cb629510bdee5061f
Azeem Bashir <azeembashir.33@gmail.com>
2024-08-25 06:54:00 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,68 @@
+// Q1
+// alert("Hello world");
+
+// Q2
+// alert("Error! Please enter a valid password");
+// Q3
+// alert("Welcome to JS world...");
+
+//q4
+// alert("Welcome to JS Land...");
+
+// alert("Happy Coding! Geo or jeene do.");
+
+//q5
+// alert("Hello... I can run JS through my web browser's console");
+// alert("Hello world");
+
+// Chapter 2
+
+//q1
+// var username;
+// username = " Azeem Bashir";
+
+//q2
+// var myName;
+// myName = "Azeem Bashir";
+
+//q3
+// var message;
+// message = "Hello World";
+// alert(message);
+
+//q4
+// var studentName;
+// var age;
+// var completionMessage;
+
+// studentName="Rizwan";
+// age = "40 years old";
+// completionMessage = "Web and Mobile Application Development";
+
+// alert(studentName);
+// alert(age);
+// alert(completionMessage);
+
+//q5
+// var message="PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(message)
+
+//q6
+// var email;
+// email="azeembashir.33@gmail.com";
+// alert("My email address is "+ email)
+
+//q7
+var book;
+book = "I am trying to learn from the Book A smarter\n and this is good way to learn";
+alert(book);
+
+//q8
+var message;
+message = "Yah ! I can do this and will be the part of IT Industry";
+alert(message);
+
+//q9
+var message;
+message = "---------any symbol---------";
+alert(message);
